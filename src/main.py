4319bea881a093c5ddc0088e19a6c57569bb0dea
Antoine Augusti <antoine@augusti.fr>
2014-08-19 11:25:23 +0200

    Print the score after fitting the SVC

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from quotesReader import *
 from sklearn import svm, cross_validation
 
-qr = QuotesReader('data/dummyQuotes.csv')
+qr = QuotesReader('data/realQuotes.csv')
 approved, wordPosition = qr.getApprovedAndWordPosition()
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(wordPosition, approved, test_size=0.4, random_state=0)
 clf = svm.SVC(kernel = "rbf")
 clf.fit(X_train, y_train)
+print clf.score(X_test, y_test)
