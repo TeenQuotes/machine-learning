4025d0b47d6afd26db7a30e4c3b3dc2ded0b7c33
Antoine Augusti <antoine@augusti.fr>
2014-12-27 19:37:36 +0100

    Change indentation

--- src/tag-quotes.py
+++ src/tag-quotes.py
@@ -19,26 +19,20 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(mess
 
 # Parse CLI arguments
 op = OptionParser()
-op.add_option("--no-minibatch", action="store_false", dest="minibatch", default=True,
-							help="Use ordinary k-means algorithm (in batch mode).")
-op.add_option("--no-idf", action="store_false", dest="use_idf", default=True,
-							help="Disable Inverse Document Frequency feature weighting.")
-op.add_option("--use-hashing", action="store_true", default=False,
-							help="Use a hashing feature vectorizer")
-op.add_option("--n-features", type=int, default=10000,
-							help="Maximum number of features (dimensions) to extract from text.")
-op.add_option("--nb-clusters", type=int, default=5,
-							help="The number of categories we want to create")
-op.add_option("--verbose", action="store_true", dest="verbose", default=False,
-							help="Print progress reports inside k-means algorithm.")
+op.add_option("--no-minibatch", action="store_false", dest="minibatch", default=True, help="Use ordinary k-means algorithm (in batch mode).")
+op.add_option("--no-idf", action="store_false", dest="use_idf", default=True, help="Disable Inverse Document Frequency feature weighting.")
+op.add_option("--use-hashing", action="store_true", default=False, help="Use a hashing feature vectorizer")
+op.add_option("--n-features", type=int, default=10000, help="Maximum number of features (dimensions) to extract from text.")
+op.add_option("--nb-clusters", type=int, default=10, help="The number of categories we want to create")
+op.add_option("--verbose", action="store_true", dest="verbose", default=False, help="Print progress reports inside k-means algorithm.")
 
 print(__doc__)
 op.print_help()
 
 (opts, args) = op.parse_args()
 if len(args) > 0:
-		op.error("This script takes no arguments.")
-		sys.exit(1)
+	op.error("This script takes no arguments.")
+	sys.exit(1)
 
 ###############################################################################
 # Read quotes
@@ -55,22 +49,22 @@ print()
 print("Extracting features from the dataset using a sparse vectorizer")
 t0 = time()
 if opts.use_hashing:
-		if opts.use_idf:
-				# Perform an IDF normalization on the output of HashingVectorizer
-				hasher = HashingVectorizer(n_features=opts.n_features,
-																	 stop_words='english', non_negative=True,
-																	 norm=None, binary=False)
-				vectorizer = make_pipeline(hasher, TfidfTransformer())
-		else:
-				vectorizer = HashingVectorizer(n_features=opts.n_features,
-																			 stop_words='english',
-																			 non_negative=False, norm='l2',
-																			 binary=False)
+	if opts.use_idf:
+		# Perform an IDF normalization on the output of HashingVectorizer
+		hasher = HashingVectorizer(n_features=opts.n_features,
+		stop_words='english', non_negative=True,
+		norm=None, binary=False)
+		vectorizer = make_pipeline(hasher, TfidfTransformer())
+	else:
+		vectorizer = HashingVectorizer(n_features=opts.n_features,
+		stop_words='english',
+		non_negative=False, norm='l2',
+		binary=False)
 else:
-		vectorizer = TfidfVectorizer(max_df=0.5, max_features=opts.n_features,
-																 min_df=2, stop_words='english',
-																 use_idf=opts.use_idf)
-X = vectorizer.fit_transform(dataset)
+	vectorizer = TfidfVectorizer(max_df=0.5, max_features=opts.n_features,
+	min_df=2, stop_words='english',
+	use_idf=opts.use_idf)
+	X = vectorizer.fit_transform(dataset)
 
 print("Done in %fs" % (time() - t0))
 print("n_samples: %d, n_features: %d" % X.shape)
@@ -80,11 +74,11 @@ print()
 # Do the actual clustering
 
 if opts.minibatch:
-		km = MiniBatchKMeans(n_clusters=nbClusters, init='k-means++', n_init=1,
-												 init_size=1000, batch_size=1000, verbose=opts.verbose)
+	km = MiniBatchKMeans(n_clusters=nbClusters, init='k-means++', n_init=1,
+	init_size=1000, batch_size=1000, verbose=opts.verbose)
 else:
-		km = KMeans(n_clusters=nbClusters, init='k-means++', max_iter=100, n_init=1,
-								verbose=opts.verbose)
+	km = KMeans(n_clusters=nbClusters, init='k-means++', max_iter=100, n_init=1,
+	verbose=opts.verbose)
 
 print("Clustering sparse data with %s" % km)
 t0 = time()
@@ -100,15 +94,14 @@ for prediction in km.labels_:
 		clusters[prediction] = [k]
 	else:
 		clusters[prediction].append(k)
-
 	k += 1
 
 if not opts.use_hashing:
-		print("Top terms per cluster:")
-		order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-		terms = vectorizer.get_feature_names()
-		for i in range(nbClusters):
-				print("Cluster %d (%i elements):" % (i, len(clusters[i])), end='')
-				for ind in order_centroids[i, :10]:
-						print(' %s' % terms[ind], end='')
-				print()
+	print("Top terms per cluster:")
+	order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+	terms = vectorizer.get_feature_names()
+	for i in range(nbClusters):
+		print("Cluster %d (%i elements):" % (i, len(clusters[i])), end='')
+		for ind in order_centroids[i, :10]:
+			print(' %s' % terms[ind], end='')
+		print()
